97f0deaaa49315bf104c558d7e4a1f451aaecb06
thedariusz <thedariusz@gmail.com>
2022-01-10 19:36:43 +0100

    Initial commit

---
+++ src/main/java/com/thedariusz/ws/api/ApiGatewayDemo/ApiGatewayDemoApplication.java
@@ -0,0 +1,13 @@
+package com.thedariusz.ws.api.ApiGatewayDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayDemoApplication.class, args);
+    }
+
+}
